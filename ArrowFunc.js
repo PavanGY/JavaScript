99a974e2fa51d31cb3a91c19bf75434c9e63353c
Pavan Ganesh <99949619+PavanGY@users.noreply.github.com>
2022-12-19 12:08:57 +0530

    Add files via upload

---
+++ ArrowFunc.js
@@ -0,0 +1,13 @@
+let greet = (user) => {
+    console.log("hello world and " +user);
+    return 1;
+}
+
+console.log(greet('Pavan'))
+
+//More Below
+
+let add= (num1,num2) => (num1+num2)
+
+let result = add(5,6)
+console.log(result)
---
+++ FuncDec.js
@@ -0,0 +1,6 @@
+console.log(SquareNum(2));
+function SquareNum(num){
+    return num*num;
+}
+
+ 
